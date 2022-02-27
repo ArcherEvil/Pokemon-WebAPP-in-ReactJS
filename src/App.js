fa070ab4217cf038c9a62f9c9d27a3832b12df50
Archer <archerevil2005@gmail.com>
2022-02-27 15:49:38 +0000

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useState } from 'react';
 
 function App() {
+
+  const [PokemonList, setPokemonList] = useState([])
+  const url = 'https://pokeapi.co/api/v2/pokemon?limit=100'
+  const list = []
+  
+  axios.get(url).then((response) => {
+    setPokemonList(response.data.results)
+    console.log(response.data.results)
+  })
+
+  
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {PokemonList.map(pokemon => <div>{pokemon.name}</div>)}
     </div>
   );
 }
