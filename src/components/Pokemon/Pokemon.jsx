0d8dffc451c863c22fb8b066d03c682dea9ead6d
Archer <archerevil2005@gmail.com>
2022-03-02 12:29:10 +0000

    :lipstick: - each pokemon have a proper page

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -7,69 +7,121 @@ const Pokemon = () => {
 
     const param = useParams().name
     const [Pokemon, SetPokemon] = useState({
-        name: param,
+        name: '',
+        img: '',
         weight: 0,
-        abilities: [],
+        abilities: 0,
+        height: 0,
+        id: 0,
         is_default: false,
-        types: [],
-        stats : {
-            hp: 0,
-            speed: 0,
-            attack: 0,
-            defense: 0
+        types: ['', ''],
+        sprites : [''],
+        stats: {
         }
-
-
     })
-    const [img, setimg] = useState('')
+
     useEffect(() => {
         const url = 'https://pokeapi.co/api/v2/pokemon/' + param;
         axios.get(url).then((response) => {
-            setimg(response.data.sprites.other['official-artwork'].front_default)
             const data = response.data
             console.log(data)
+            console.log('Data is Above \n \n')
 
 
             // Determening Abilities
-            let abilities = []
+            let abilitieslist = []
             for (var i = 0; i < data.abilities.length; i++) {
                 if (data.abilities[i].is_hidden == true) {
-                    abilities.push(data.abilities[i].ability.name + " (hidden)")
+                    abilitieslist.push(data.abilities[i].ability.name + " (hidden)")
                 }
                 else {
-                    abilities.push(data.abilities[i].ability.name)
+                    abilitieslist.push(data.abilities[i].ability.name)
                 }
             }
 
             // Determening Types
-            let types = []
+            let typeslist = []
             for (var i = 0; i < data.types.length; i++) {
-                types.push(data.types[i].type.name)
+                typeslist.push(data.types[i].type.name)
             }
 
-            const object = {
+            
+            // Determening sprites
+            let spriteslist = []
+            for (var i = 0; i< Object.entries(data.sprites).length; i++) {
+                if (Object.entries(data.sprites)[i][1] != null & typeof Object.entries(data.sprites)[i][1] != 'object') {
+                spriteslist.push(Object.entries(data.sprites)[i][1])
+            }}
+
+            // Determening Stats
+            let statslist = {}
+            for (var i = 0; i< data.stats.length; i++ ) {
+                statslist[data.stats[i].stat.name] = data.stats[i].base_stat
+            }
+
+            const Dict = {
                 name: param,
+                img: data.sprites.other['official-artwork'].front_default,
                 weight: data.weight,
-                abilities: abilities,
+                abilities: abilitieslist,
                 height: data.height,
                 id: data.id,
                 is_default: data.is_default,
-                types: types,
-                
-
-
+                types: typeslist,
+                sprites : spriteslist,
+                stats: statslist
             }
-            console.log(object)
+            SetPokemon(Dict)
+            console.log(Pokemon)
         })
     }, [])
+
 
     return (
         <main>
             <div className='container'>
-
+                <div className={[Pokemon.types[0], 'imgcontainer'].join(' ')}>
+                    <img className={[Pokemon.types[0]]} src={Pokemon.img}/>
+                <p>{Pokemon.name}</p>
+                </div>
             </div>
         </main>
     )
 }
 
 export default Pokemon
+
+/* API Output:
+{
+  "name": "geodude",
+  "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/74.png",
+  "weight": 200,
+  "abilities": [
+    "rock-head",
+    "sturdy",
+    "sand-veil (hidden)"
+  ],
+  "height": 4,
+  "id": 74,
+  "is_default": true,
+  "types": [
+    "rock",
+    "ground"
+  ],
+  "sprites": [
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/74.png",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/shiny/74.png",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/74.png",
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/74.png"
+  ],
+  "stats": {
+    "hp": 40,
+    "attack": 80,
+    "defense": 100,
+    "special-attack": 30,
+    "special-defense": 30,
+    "speed": 20
+  }
+}
+
+*/
